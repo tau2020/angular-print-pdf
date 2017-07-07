9150a72a3b2f684695d0eb8c8b4c7bde93c88bb5
mmakombe <mmakombe@zimhisp.dev>
2017-07-07 12:32:55 -0700

    updated for ehr

--- dist/saveHtmlToPdf.js
+++ dist/saveHtmlToPdf.js
@@ -90,23 +90,27 @@ angular.module('htmlToPdfSave')
 
 					function convert(theElement , id) {
 						var quotes = $('div[pdf-save-content='+id+']')[0];
+
+						
+
+
 				        html2canvas(quotes, {
 				            onrendered: function(canvas) {
-				            var pdf = new jsPDF('p', 'px', [1190.55, 1683.78]);
-				            for (var i = 0; i <= quotes.clientHeight/1600 ; i++) {
+				            var pdf = new jsPDF('p', 'px', [841.89, 1190.55]);
+				            for (var i = 0; i <= quotes.clientHeight/1190.55 ; i++) {
 				                var srcImg  = canvas;
 				                var sX      = 0;
-				                var sY      =2270 *i; // start2270  pixels down for every new page
-				                var sWidth  = 1600  ;
-				                var sHeight =2270 ;
+				                var sY      =1750 *i; // start1190.55  pixels down for every new page
+				                var sWidth  = 1100  ;
+				                var sHeight =1750 ;
 				                var dX      = 0;
 				                var dY      = 0;
-				                var dWidth  = 1600  ;
-				                var dHeight =2270 ;
+				                var dWidth  = 1100  ;
+				                var dHeight =1750 ;
 
 				                window.onePageCanvas = document.createElement("canvas");
-				                onePageCanvas.setAttribute('width', 1600  );
-				                onePageCanvas.setAttribute('height',2270 );
+				                onePageCanvas.setAttribute('width', 1100  );
+				                onePageCanvas.setAttribute('height',1750 );
 				                var ctx = onePageCanvas.getContext('2d');
 				                // details on this usage of this function: 
 				                // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images#Slicing
@@ -121,7 +125,7 @@ angular.module('htmlToPdfSave')
 				                //! If we're on anything other than the first page,
 				                // add another page
 				                if (i > 0) {
-				                    pdf.addPage(1190.55, 1683.78); //8.5" x 11" in pts (in*72)
+				                    pdf.addPage(841.89, 1190.55); //8.5" x 11" in pts (in*72)
 				                }
 				                //! now we declare that we're working on that page
 				                pdf.setPage(i+1);
@@ -135,7 +139,7 @@ angular.module('htmlToPdfSave')
 				      });
 					/*	var element = $('[pdf-save-content='+id+']') ,
 						cache_width = element.width(),
-						 a2  =[ 1600, 1190.55];  // for a2 size paper width and height
+						 a2  =[ 841.89, 841.89];  // for a2 size paper width and height
 
 
 						 $('body').scrollTop(0);
@@ -161,7 +165,7 @@ angular.module('htmlToPdfSave')
 					/*	function getCanvas(){
 							element.width((a2[0]*1.33333) -80).css('max-width','none');
 							return html2canvas(element,{
-								imageTimeout:2270,
+								imageTimeout:1190.55,
 								removeContainer:true
 							});
 						}*/
